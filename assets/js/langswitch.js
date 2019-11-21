f4971b4411497bebf8dfda132057de0c8ccc87a4
Shiporin, Grigoriy <grigoriy.shiporin@x5.ru>
2019-11-21 15:23:26 +0300

    Link change fix

--- assets/js/langswitch.js
+++ assets/js/langswitch.js
@@ -1,6 +1,7 @@
 //https://habr.com/ru/sandbox/36761/
 
 var LANGUAGE;
+var LANGLNK;
 
 function redrawlng(lang) {
         $.ajax({
@@ -25,4 +26,24 @@ function redrawlng(lang) {
                 });
             }
         });
+
+    $.ajax({
+
+        url: 'languages/hl' + lang + '.json', //тянем файл с языком
+        dataType: 'json',
+        success: function (response) {
+            LANGLNK = response; //записываем в глобальную переменную, а вдруг пригодиться
+            $('body').find("[hl]").each(function () //ищем все элементы с атрибутом
+            {
+                var hl = LANGLNK[$(this).attr('hl')]; //берем нужное значение по атрибуту lng
+                var tag = $(this)[0].tagName.toLowerCase();
+                switch (tag) //узнаем название тега
+                {
+                    default:
+                        $(this).attr("href", hl);
+                        break;
+                }
+            });
+        }
+    });
     };
